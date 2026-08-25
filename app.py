547233b449e5d1a5388d097754d0a755f4687aeb
dep0r <syakiralamsyah49@gmail.com>
2026-08-25 21:50:35 +0700

    coba streamlit button

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.write('Hello world!')
+st.header('st.button')
+
+# cara kerja st.button()
+# argumen adalah display buttonnya, lalu nanti akan digantikan true atau false.
+
+if st.button('Say hello'):
+    st.write('Hello there')
+else: 
+    st.write('Goodbye')
