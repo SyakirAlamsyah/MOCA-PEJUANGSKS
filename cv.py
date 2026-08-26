18d748f49e02f125ceb9390eaba3337a05a563cb
dep0r <syakiralamsyah49@gmail.com>
2026-08-26 12:30:57 +0700

    perbaiki kamera dua

--- cv.py
+++ cv.py
@@ -1,11 +1,24 @@
 import cv2
 from ultralytics import YOLO
 
+def gstreamer_pipeline(capture_width=1280, capture_height=720, display_width=1280, display_height=720, framerate=30, flip_method=0):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        f"width=(int){capture_width}, height=(int){capture_height}, "
+        f"framerate=(fraction){framerate}/1 ! "
+        f"nvvidconv flip-method={flip_method} ! "
+        f"video/x-raw, width=(int){display_width}, height=(int){display_height}, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+    )
+
+
 # Path Modelnya
 model = YOLO("runs/detect/train/weights/best.pt")
 
 #  Setup Computer Vision (Kamera)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 
 #Setup Kondisi
 classcon = ['no head cap', 'no mark', 'lab coat', 'no goggle']
