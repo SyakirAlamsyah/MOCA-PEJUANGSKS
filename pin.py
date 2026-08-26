f33eb9657d494478d4c233f025418d7b59a0d615
SyakirAlamsyah <syakiralamsyah49@gmail.com>
2026-08-26 18:33:40 +0700

    Setup percobaan Buzzer

--- pin.py
+++ pin.py
@@ -1,22 +1,29 @@
 import Jetson.GPIO as io
 import time
 
-ledp = 7
+# ledp = 7
+
+# io.setmode(io.BOARD)
+
+# io.setup(ledp, io.OUT, initial=io.LOW)
+
+# print("CTRL+C untuk berhenti")
+
+# try:
+#     while True:
+#         io.output(ledp, io.HIGH)
+#         time.sleep(2)
+
+#         io.output(ledp, io.LOW)
+#         time.sleep(2)
+
+# except KeyboardInterrupt:
+#     io.output(ledp, io.LOW)
+#     io.cleanup()
+
+buzp = 7
 
 io.setmode(io.BOARD)
 
-io.setup(ledp, io.OUT, initial=io.LOW)
+io.setup(buzp, io.OUT, initial=io.LOW)
 
-print("CTRL+C untuk berhenti")
-
-try:
-    while True:
-        io.output(ledp, io.HIGH)
-        time.sleep(2)
-
-        io.output(ledp, io.LOW)
-        time.sleep(2)
-
-except KeyboardInterrupt:
-    io.output(ledp, io.LOW)
-    io.cleanup()
