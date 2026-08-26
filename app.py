f453d00a55ad8c463107989722ceb45c8d0d0ca5
SyakirAlamsyah <syakiralamsyah49@gmail.com>
2026-08-26 10:06:05 +0700

    test jetson

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.header('st.button')
 # cara kerja st.button()
 # argumen adalah display buttonnya, lalu nanti akan digantikan true atau false.
 
-if st.button('Say hello'):
+if st.button('Dari Jetson'):
     st.write('Hello there')
 else: 
-    st.write('Goodbye')
+    st.write('halo Jetson')
