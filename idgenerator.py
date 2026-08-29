52b5b46ae970f35558a8d3b34ae3a56d88e5827d
SyakirAlamsyah <syakiralamsyah49@gmail.com>
2026-08-29 14:47:30 +0700

    perbaiki id generator

--- idgenerator.py
+++ idgenerator.py
@@ -1,18 +1,6 @@
 from ultralytics import YOLO
 
-model = ""
-opsi = int(input("Pilih model apa?(1/2/3/4)"))
+opsi = input("Pilih model apa? ")
 
-if opsi == 1:
-    model == "model"
-elif opsi == 2:
-    model == "model1"
-elif opsi == 3:
-    model == "best"
-elif opsi == 4:
-    model == "model3"
-else:
-    print(f"{opsi} tidak valid")
-
-models = YOLO(f"models/{model}.engine", task="detect")
-print(models.names)
+model = YOLO("models/"+opsi+".engine", task="detect")
+print(model.names)
